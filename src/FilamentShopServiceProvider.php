d92450ccec0858a522b2dc5f6c820e09c7e37ef3
igwen6w <igwen6w@gmail.com>
2023-10-24 14:55:51 +0800

    .gitignore

---
+++ src/FilamentShopServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Igwen6w\FilamentShop;
+
+use Illuminate\Support\ServiceProvider;
+
+class FilamentShopServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+
+    }
+}
